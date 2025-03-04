1c0dae9b448af784eb1ae0bb9d96ec45c31e9bf2
Mohit-Pala <palav@appstate.edu>
2025-03-04 15:43:37 -0500

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { SearchListComponent } from './components/search-list/search-list.component';
+import { DisplayComponent } from './components/search-list/display/display.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    {path: '', component: MainComponent},
+    {path: 'search', component: SearchListComponent},
+    {path: 'search/:id', component: DisplayComponent}
+];
