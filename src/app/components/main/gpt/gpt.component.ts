e7a84a5cbd541321eb91fbdc0f200029abb3c16a
Mohit-Pala <palav@appstate.edu>
2025-03-04 15:14:00 -0500

    added required components

---
+++ src/app/components/main/gpt/gpt.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gpt',
+  imports: [],
+  templateUrl: './gpt.component.html',
+  styleUrl: './gpt.component.css'
+})
+export class GptComponent {
+
+}
