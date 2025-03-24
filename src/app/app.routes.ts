08aa5e965d5bf18c5889df6081098278b0a0d2ad
Mohit-Pala <vmohit.pala@.com>
2025-03-23 23:07:42 -0400

    about and tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { SearchListComponent } from './components/search-list/search-list.component';
 import { DisplayComponent } from './components/search-list/display/display.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [ 
     {path: '', component: MainComponent},
     {path: 'search', component: SearchListComponent},
-    {path: 'search/:id', component: DisplayComponent}
+    {path: 'search/:id', component: DisplayComponent},
+    {path: 'about', component: AboutComponent}
 ];
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -8,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  // version by weeks i think 
+
+  projectName: string = 'News Analyzer'
+
+  version = '0.7.0'
+
+  githubPage = 'https://github.com/Mohit-Pala/nws-frontend'
+
+  contributors = [
+    { name: 'Mohit Pala', github: 'https://github.com/mohit-pala' },
+    { name: 'Rachel', github: 'https://github.com/rachelking12' },
+  ]
+
+  frontend = [
+    { name: 'Angular', link: 'https://angular.io/' },
+    { name: 'Plotly', link: 'https://plotly.com/javascript/' },
+    { name: 'Tailwind', link: 'https://tailwindcss.com/' },
+    { name: 'Firebase', link: 'https://firebase.google.com/' },
+  ]
+
+  backend = [
+    // Python
+    { name: 'Flask', link: 'https://flask.palletsprojects.com/en/stable/' },
+    { name: 'Pytorch', link: 'https://pytorch.org/' },
+    { name: 'Hugging Face Transformers', link: 'https://huggingface.co/' },
+
+    // Web 
+    { name: 'Vertex AI', link: 'https://cloud.google.com/vertex-ai' },
+    { name: 'OpenAI', link: 'https://openai.com/' },
+    { name: 'Google Cloud', link: 'https://cloud.google.com/' },
+    { name: 'Firestore Database', link: 'https://firebase.google.com/docs/firestore' },
+  ]
+
+  modelsUsed = [
+    'Sentiment Analysis',
+    'Emotion Analysis',
+    'GPT-3',
+    'Gemini',
+  ]
+
+  devDetails = {
+    madeFor: 'Capstone Project, Appalachian State University, Computer Science',
+    madeBy: 'Mohit Pala, Rachel',
+    startDate: 'Feb 2025',
+    endDate: 'April 2025',
+  }
+
+  // aboutThisProject = [
+  //   'This project is a news analyzer that uses machine learning models to analyze news articles and provide insights on the sentiment and emotions of the article.',
+  //   'The project uses a variety of machine learning models to analyze the news articles, including a sentiment analysis model, an emotion analysis and LLMs like GPT-3 and Gemini.',
+  // ]
 }
