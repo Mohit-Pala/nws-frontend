3aa4e19ce4025317f217affea454628a0e2158ae
Mohit-Pala <vmohit.pala@.com>
2025-03-25 14:49:48 -0400

    async post request for rest api

---
+++ src/app/services/rest-api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestApiService {
+
+  constructor() { }
+
+  async getOutput(title: string, article: string) {
+    const getRequest = 'https://api.susamongus.com'
+    const response = await fetch(getRequest, {
+      method: 'POST',
+      headers: {
+
+      },
+      body: JSON.stringify({
+        title: title,
+        article: article
+      })
+    })
+
+    if (!response.ok) {
+      throw new Error('Failed to get output from the backend')
+    }
+
+    return await response.json()
+  }
+}
