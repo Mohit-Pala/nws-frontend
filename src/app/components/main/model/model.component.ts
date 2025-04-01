13a349b217b23f6044a40cf19ecef3779a578c46
Rachel <rachel99.king@gmail.com>
2025-04-01 16:27:45 -0400

    working on deepseek output

--- src/app/components/main/model/model.component.ts
+++ src/app/components/main/model/model.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, inject, OnInit } from '@angular/core';
 import { PlotService } from '../../../services/plot.service';
 import { CommonModule } from '@angular/common';
+import { KeyValueCustom } from '../../../models/key-value-custom.model';
 
 @Component({
   selector: 'app-model',
@@ -9,14 +10,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './model.component.html',
   styleUrl: './model.component.css'
 })
-export class ModelComponent implements OnChanges {
+export class ModelComponent implements OnChanges, OnInit {
   @Input() apiData: any;
 
   plotly = inject(PlotService);
   emotions: any[] = [];
   sentimentData: any[] = [];
+  guageSimilarity: number = 0;
+  similarityData: KeyValueCustom[] = [
+    { name: 'Cosine Similarity', value: 0.85 }, // higher is better
+    { name: 'TF-IDF Similarity', value: 0.72 }, // higher is better
+    { name: 'Jaccard (Words)', value: 0.45 }, // higher is better
+    { name: 'Jaccard (Bigrams)', value: 0.32 }, // higher is better
+    { name: 'Normalized Edit Distance', value: 0.28 } // lower is better
+  ];
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.plotly.makeRadarChart(this.similarityData, 'Similarity Analysis', 'similarity-radar');
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['apiData']) {
@@ -38,6 +51,10 @@ export class ModelComponent implements OnChanges {
     this.sentimentData = this.processSentimentData(this.apiData.sentiment);
     console.log("Sentiment data:", this.sentimentData);
 
+    // Handle the similarity data
+    this.guageSimilarity = this.apiData.guageSimilarity || 0;
+    console.log("Gauge data:", this.guageSimilarity)
+
     if (this.emotions.length > 0) {
       this.plotly.makeBarGraph(this.emotions, 'Emotional Analysis', 'Emotion', 'Score', 'bar-graph');
     } else {
@@ -49,9 +66,8 @@ export class ModelComponent implements OnChanges {
     } else {
       console.warn("No sentiment data to display");
     }
+    this.plotly.makeGaugeChart(this.guageSimilarity, 'Text Similarity', 'Cosine Similarity', 'similarity-gauge');
   }
-
-  
 
   processSentimentData(sentiment: any): any[] {
     if (!sentiment || !sentiment.sentiment || !sentiment.score) {
