fedc5283e503cd1d2fbb25e35a43fe11bb03cd36
Rachel <rachel99.king@gmail.com>
2025-04-28 19:05:00 -0400

    got baseline up

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ import Levenshtein
 import os
 
 app = Flask(__name__)
-# CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 # Load the sentiment analysis model
@@ -221,4 +221,4 @@ def submit_data():
 
 if __name__ == '__main__':
     # app.run(debug=True, port=5001)
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
