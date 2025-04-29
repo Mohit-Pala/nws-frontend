efc473a7df03249d423f89bfe591567c6f99b79f
Mohit-Pala <vmohit.pala@.com>
2025-04-29 12:09:24 -0400

    bruhhhhh

--- src/app/components/search-list/search-list.component.ts
+++ src/app/components/search-list/search-list.component.ts
@@ -26,6 +26,7 @@ export class SearchListComponent implements OnInit {
   firestore = inject(FirestoreService)
   searchTerm = ''
   items: FsSearch[] = []
+  searches: FsSearch[] = []
   signedIn = true
 
 
@@ -58,23 +59,39 @@ export class SearchListComponent implements OnInit {
   reset() {
     this.searchTerm = ''
     this.items = []
+    this.searches = []
     this.getAllData()
   }
 
-  searchByKeyword() {
-    this.firestore.searchTitleSubstring(this.searchTerm).then((res) => {
-      if(!res) {
-        return
-      }
-      if(res.length == 0) {
-        alert('No results found')
-        this.getAllData()
-        return
-      }
-      this.items = res
-    }).catch((err) => {
-      alert(err)
+  filterBySubstring() {
+    // fileter items list by the substring of title
+    if(this.searchTerm.length < 3) {
+      alert('Please enter at least 3 characters')
+      return
+    }
+    this.searches = this.items.filter(item => {item.item.title.includes(this.searchTerm.toLowerCase())})
+    if(this.searches.length == 0) {
+      alert('No results found')
       this.getAllData()
-    })
+      return
+    }
   }
+ 
+
+  // searchByKeyword() {
+  //   this.firestore.searchTitleSubstring(this.searchTerm).then((res) => {
+  //     if(!res) {
+  //       return
+  //     }
+  //     if(res.length == 0) {
+  //       alert('No results found')
+  //       this.getAllData()
+  //       return
+  //     }
+  //     this.items = res
+  //   }).catch((err) => {
+  //     alert(err)
+  //     this.getAllData()
+  //   })
+  // }
 }
